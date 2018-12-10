37eeac6dbe52ec25cac7358153167e32121245dd
wmlbuaa <wmlbuaa@163.com>
2018-12-10 17:27:22 +0800

    Start

---
+++ Graph.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+top = tk.Tk()
+top.title("Simulate")
+top.geometry("720x480")
+top.resizable(width=False, height=False)
+
+top.mainloop()
---
+++ Simulate.py
@@ -0,0 +1,4 @@
+import Graph
+import tensorflow as tf
+import numpy as np
+import pandas as pd
