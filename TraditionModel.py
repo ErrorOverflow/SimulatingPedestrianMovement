de310e9e90a2d752c95fe91ff2cf0b7a2da59588
wmlbuaa <wmlbuaa@163.com>
2018-12-26 14:08:01 +0800

    Right

--- TraditionModel.py
+++ TraditionModel.py
@@ -36,41 +36,55 @@ def gravity(loc):  # 驱动力
         x_bias = 12.46 - loc[0]
         y_bias = (7.39 + 6.39) / 2 - loc[1]
         z_bias = np.sqrt(np.square(x_bias) + np.square(y_bias))
-        grav[0] = x_bias / z_bias * 0.2
-        grav[1] = y_bias / z_bias * 0.2
+        grav[0] = x_bias / z_bias * 0.06
+        grav[1] = y_bias / z_bias * 0.06
     elif 9.33 <= loc[0] < 9.91:
-        grav[0] = 0.2
+        grav[0] = 0.06
         grav[1] = 0
     else:
         if loc[1] >= (7.50 + 6.32) / 2:
             x_bias = 9.33 - loc[0]
             y_bias = (7.50 + 0.3) - loc[1]
             z_bias = np.sqrt(np.square(x_bias) + np.square(y_bias))
-            grav[0] = x_bias / z_bias * 0.2
-            grav[1] = y_bias / z_bias * 0.2
+            grav[0] = x_bias / z_bias * 0.06
+            grav[1] = y_bias / z_bias * 0.06
         else:
             x_bias = 9.33 - loc[0]
             y_bias = (6.32 - 0.3) - loc[1]
             z_bias = np.sqrt(np.square(x_bias) + np.square(y_bias))
-            grav[0] = x_bias / z_bias * 0.2
-            grav[1] = y_bias / z_bias * 0.2
+            grav[0] = x_bias / z_bias * 0.06
+            grav[1] = y_bias / z_bias * 0.06
     return grav
+
+
+'''
+        self.C.create_rectangle((12.46 - (3.94 - 3.41)) * 50, 7.39 * 50, 12.46 * 50, 10.28 * 50, fill="gray")
+        self.C.create_rectangle((12.46 - (3.94 - 3.41)) * 50, 3.21 * 50, 12.46 * 50, 6.39 * 50, fill="gray")
+'''
 
 
 def illegal_judge(loc, force):  # 墙排斥
     if (9.33 - 0.2) <= loc[0] <= 9.91 and (6.32 - 0.2) <= loc[1] <= (7.50 + 0.2):
         if loc[0] <= 9.33:
             force[0] = 0
-            force[1] = (loc[1] - (6.32 + 7.50) / 2) * random.random() * 0.05
         else:
             force[1] = 0
-            force[0] += random.random() * 0.05
         return force
+    if (12.46 - (3.94 - 3.41) - 0.2) <= loc[0] <= 12.46 and 7.39 - 0.2 <= loc[1] <= 10.28:
+        if loc[0] <= 12.46:
+            force[0] = 0
+        elif force[1] > 0:
+            force[1] = 0
+    if (12.46 - (3.94 - 3.41) - 0.2) <= loc[0] <= 12.46 and 3.21 - 0.2 <= loc[1] <= 6.39:
+        if loc[0] <= 12.46:
+            force[0] = 0
+        elif force[1] < 0:
+            force[1] = 0
     return force
 
 
 def avoid_hit(force, pre_v, num):  # 行人碰撞
-    near = [0.00 for i in range(2)]
+    near = [0.00 for _ in range(2)]
     i = 0
     while i < 24:
         if i == num:
@@ -87,11 +101,24 @@ def avoid_hit(force, pre_v, num):  # 行人碰撞
         return force
     force[0] = (force[0] / 0.1 + near[0] / z_bias) * 0.05
     force[1] = (force[1] / 0.1 + near[1] / z_bias) * 0.05
+
+    next = [0.00 for _ in range(2)]
+    i = 0
+    while i < 24:
+        next[0] = pre_v[0] + force[0]
+        next[1] = pre_v[1] + force[1]
+        if i == num:
+            i += 1
+            continue
+        if lib.distance(next, location[i]) <= 0.2 :
+            force[0] = 0
+            force[1] = 0
+        i += 1
     return force
 
 
 def kill(loc, num):
-    if loc[0] >= (12.46 - (3.94 - 3.41)):
+    if loc[0] >= 12.46:
         isKill[num] = 1
     return 0
 
@@ -116,10 +143,10 @@ for round in range(2000):
     for i in range(24):  # 依次挪动24个行人
         kill(location[i], i)
         if isKill[i]:
-            location[i] = [15, 8]
+            location[i] = [30, 30]
             continue
         force = gravity(location[i])
         location[i] = move(force, location[i], i)
     ui.init(location)
     ui.barrier()
-    time.sleep(0.2)
+    time.sleep(0.1)
